d9efdd620827b937b7652cff785f00d7bf71bfcd
Boken Lin <b@boken.org>
2026-04-25 00:15:18 +0000

    Q0 HE: swap Z and Q chord assignments

--- keyboards/keychron/q0_he/encoder/keymaps/via/keymap.c
+++ keyboards/keychron/q0_he/encoder/keymaps/via/keymap.c
@@ -17,7 +17,7 @@ enum layers { BASE, CUSTOM, NAV, FN };
     X(w, KC_W, KC_H, KC_S) X(b, KC_B, KC_L, KC_N) \
     X(v, KC_V, KC_A, KC_O) X(k, KC_K, KC_L, KC_R) \
     X(j, KC_J, KC_T, KC_S) X(x, KC_X, KC_T, KC_H) \
-    X(z, KC_Z, KC_P, KC_N) X(q, KC_Q, KC_D, KC_S)
+    X(z, KC_Q, KC_P, KC_N) X(q, KC_Z, KC_D, KC_S)
 
 #define FOR_EACH_ALT_CHORD(X) \
     X(e, KC_E, KC_P, KC_R) X(p, KC_P, KC_D, KC_H)
